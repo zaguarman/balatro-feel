fc20e9de297dcfef47976ac4dc28c243df3b8c81
David Dumitrascu <zaguarman@gmail.com>
2024-11-21 09:42:12 +0100

    Added GameManager on runtime as a DontDestroyOnLoad game object

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,7 +2,21 @@ using UnityEngine;
 using System;
 
 public class GameManager : MonoBehaviour {
-    public static GameManager Instance { get; private set; }
+    private static GameManager instance;
+    public static GameManager Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<GameManager>();
+                if (instance == null) {
+                    var go = new GameObject("GameManager");
+                    instance = go.AddComponent<GameManager>();
+                    DontDestroyOnLoad(go);
+                }
+            }
+            return instance;
+        }
+    }
+
     public IPlayer Player1 { get; private set; }
     public IPlayer Player2 { get; private set; }
     public GameContext GameContext { get; private set; }
@@ -12,29 +26,49 @@ public class GameManager : MonoBehaviour {
     public event Action<ICreature> OnCreatureDamaged;
     public event Action<IPlayer> OnGameOver;
 
+    private bool isInitialized = false;
+
     public void Awake() {
-        if (Instance == null) {
-            Instance = this;
-            InitializeGame();
-        } else {
+        if (instance != null && instance != this) {
             Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        if (!isInitialized) {
+            InitializeGame();
+            isInitialized = true;
         }
     }
 
     private void InitializeGame() {
-        GameContext = new GameContext();
-        Player1 = new Player();
-        Player2 = new Player();
-        Player1.Opponent = Player2;
-        Player2.Opponent = Player1;
+        try {
+            GameContext = new GameContext();
+            Player1 = new Player();
+            Player2 = new Player();
+            Player1.Opponent = Player2;
+            Player2.Opponent = Player1;
 
-        GameMediator.Instance.RegisterPlayer(Player1);
-        GameMediator.Instance.RegisterPlayer(Player2);
+            GameMediator.Instance.RegisterPlayer(Player1);
+            GameMediator.Instance.RegisterPlayer(Player2);
 
-        NotifyGameStateChanged();
+            NotifyGameStateChanged();
+
+            Debug.Log("GameManager initialized successfully");
+        } catch (Exception e) {
+            Debug.LogError($"Error initializing GameManager: {e.Message}");
+            isInitialized = false;
+        }
     }
 
     public void PlayCard(CardData cardData, IPlayer player) {
+        if (!isInitialized) {
+            Debug.LogError("GameManager not properly initialized!");
+            return;
+        }
+
         Debug.Log($"Playing card: {cardData.cardName}");
         ICard card = CardFactory.CreateCard(cardData);
         card.Play(GameContext, player);
@@ -43,6 +77,11 @@ public class GameManager : MonoBehaviour {
     }
 
     public void AttackWithCreature(ICreature attacker, IPlayer attackingPlayer, ICreature target = null) {
+        if (!isInitialized) {
+            Debug.LogError("GameManager not properly initialized!");
+            return;
+        }
+
         if (target == null) {
             Debug.Log($"{attacker.Name} attacking opponent directly");
             GameContext.AddAction(new DamagePlayerAction(attackingPlayer.Opponent, attacker.Attack));
@@ -68,12 +107,29 @@ public class GameManager : MonoBehaviour {
 
     public void NotifyGameStateChanged() {
         OnGameStateChanged?.Invoke();
-        GameMediator.Instance.NotifyGameStateChanged();
+        GameMediator.Instance?.NotifyGameStateChanged();
+    }
+
+    // Reset game state
+    public void ResetGame() {
+        isInitialized = false;
+        InitializeGame();
+    }
+
+    // Scene management helper
+    public void OnSceneLoaded() {
+        if (!isInitialized) {
+            InitializeGame();
+        }
+    }
+
+    private void OnApplicationQuit() {
+        isInitialized = false;
     }
 
     public void OnDestroy() {
-        if (Instance == this) {
-            Instance = null;
+        if (instance == this) {
+            instance = null;
         }
     }
 }
